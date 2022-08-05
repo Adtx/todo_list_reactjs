cb1a0d0c69759391fdd3c10946bc2a0aa5f3abfb
Adtx <adrianodiasteixeira@gmail.com>
2022-08-05 15:33:31 +0100

    Add LocalStorage

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,51 +1,66 @@
-import React, { useState } from 'react'
-import Todo from './Todo'
-import TodoForm from './TodoForm'
+import React, { useEffect, useState } from "react";
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
 
 function TodoList() {
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos")) || []
+  );
 
-    const [todos, setTodos] = useState([])
-
-   function addTodo(todo) {
-        if(!todo.text || /^\s*$/.test(todo.text)) {
-            return 
-        }
-        const newTodos = [todo, ...todos]
-        
-        setTodos(newTodos)
+  function addTodo(todo) {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
     }
-    
-    function updateTodo(id, newValue) {
-        if(!newValue.text || /^\s*$/.test(newValue.text)) {
-            return 
-        }
-        setTodos(todos => todos.map(todo => todo.id === id ? newValue : todo))
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+  }
+
+  function updateTodo(id, newValue) {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
     }
+    setTodos((todos) =>
+      todos.map((todo) => (todo.id === id ? newValue : todo))
+    );
+  }
 
-    function removeTodo(id) {
-        setTodos(todos => todos.filter(todo => todo.id !== id))
-    }
+  function removeTodo(id) {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }
 
-    function completeTodo(id) {
+  function completeTodo(id) {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isComplete: !todo.isComplete } : todo
+      )
+    );
 
-        setTodos(todos => todos.map(todo => todo.id === id ? {...todo, isComplete: !todo.isComplete} : todo))
-
-        /* let updatedTodos = todos.map(todo => {
+    /* let updatedTodos = todos.map(todo => {
             if (todo.id === id) {
                 todo.iscomplete = !todo.isComplete
             }
             return todo
         })
         setTodos(updatedTodos) */
-    }
+  }
 
-    return (
-        <div>
-            <h1>What's the plan for today?</h1>
-            <TodoForm onSubmit={addTodo}/>
-            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo}/>
-        </div>
-    )
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <div>
+      <h1>What's the plan for today?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </div>
+  );
 }
 
-export default TodoList
+export default TodoList;
